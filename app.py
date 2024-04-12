c1e8c4d2c55d13d51f9eac22f18939ed161d6ae2
srikanthkoncherry <52325569+srikanthkoncherry@users.noreply.github.com>
2024-04-12 20:07:58 +0000

    working1

--- app.py
+++ app.py
@@ -6,16 +6,18 @@ import os
 app = Flask(__name__)
 
 # Distribution Version
-### app = Flask(__name__, static_folder='_frontend/users/dist', static_url_path='')
+app = Flask(__name__, static_folder='_frontend/users/dist', static_url_path='')
 # Add CORS support
-### CORS(app)
-### @app.route('/', methods=['GET'])
-### def defaultPage():
-###    return send_file(os.path.join(app.static_folder, 'index.html'))
+CORS(app)
+@app.route('/', methods=['GET'])
+def defaultPage():
+    print(" in APP")
+    return send_file(os.path.join(app.static_folder, 'index.html'))
 
 # create the database first
-@app.route('/createDB')
+@app.route('/createDB', methods=['GET'])
 def index():
+    print("creating DB")
     conn = sqlite3.connect('mydatabase.db')
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE users
